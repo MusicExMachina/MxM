dc3e3084cb179b578a166d48af2aafbd188e6332
PatrickCelentano <patrick_celentano@yahoo.com>
2016-09-06 13:40:48 -0400

    Solved GitHub account issue- added various Model classes.

--- src/main/java/model/Frame.java
+++ src/main/java/model/Frame.java
@@ -1,28 +1,50 @@
 package model;
-
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.TreeSet;
 
 /**
- * Created by celenp on 9/4/2016.
+ * A frame is a conceptualization of
  */
 public class Frame implements Comparable<Frame> {
 
-    TreeSet<Note> notes;
     long time;
+    TreeSet<Note> notes;
 
+    /**
+     * The constructor for a Frame. Note that Frames
+     * must be constructed with a time.
+     * @param time The timing of this frame (in ms)
+     */
+    public Frame(long time) {
+        this.time = time;
+    }
+
+    /**
+     *
+     * @return
+     */
     public long getTime() {
         return time;
     }
+
+    /**
+     * Returns a collection of all notes playing
+     * this frame, ordered by pitch- lowest to
+     * highest.
+     * @return All notes playing this frame
+     */
     /*
     public List<Note> getNotes() {
         //return new ArrayList<Note>(notes.toArray());
     }
     */
+    /**
+     * Frame comparison is useful as a means of
+     * @param o
+     * @return
+     */
     @Override
     public int compareTo(Frame o) {
-        //return getTime() > o.getTime();
+        return Long.compare(getTime(),o.getTime());
     }
 }
---
+++ src/main/java/model/Passage.java
@@ -0,0 +1,42 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.TreeSet;
+
+/**
+ * A passage is a relatively straightforward
+ * collection of frames which, perhaps most
+ * importantly, allows time-based lookup. For
+ * instance, you can find the last frame before
+ * a given time.
+ *
+ * Notes:
+ *  There can never be more than one frame at a given time point
+ *  There can never be a frame past the end or before time 0
+ */
+public class Passage {
+
+    TreeSet<Frame> frames;          // A time-ordered set of all the frames in this passage
+    ArrayList<Player> players;      // A list of all instruments playing in this passage
+
+    /**
+     * We do not add Frames directly to a Passage, but
+     * instead add Notes themselves, which automatically fill
+     * up frames.
+     */
+    public void addNote(Note note, long time) {
+
+    }
+
+    /**
+     * The only way to access the frames held in a
+     * passage is through an iterator, both for
+     * performance and encapsulation considerations
+     * Note: This is, of course, not edit safe
+     * @return an iterator to all the Frames
+     */
+    public Iterator<Frame> getFrameIterator() {
+        return frames.iterator();
+    }
+}
