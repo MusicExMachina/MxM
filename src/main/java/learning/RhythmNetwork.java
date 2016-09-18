c9ee41c0db07b206ff928c3a0f6db97deb1cb782
PatrickCelentano <patrick_celentano@yahoo.com>
2016-09-17 23:44:40 -0400

    Added the first learning interface.

---
+++ src/main/java/learning/RhythmNetwork.java
@@ -0,0 +1,18 @@
+package learning;
+
+import model.structural.Frame;
+
+import java.util.List;
+
+/**
+ * A RhythmNetwork is one which generates rhythms (duh).
+ */
+public interface RhythmNetwork {
+    /**
+     * Generates a rhythm of a given length given a seed.
+     * @param seed
+     * @param numMeasures
+     * @return
+     */
+    public List<Frame> generateRhythm(int seed, int numMeasures);
+}
---
+++ src/main/java/ui/MidiPlayer.java
@@ -0,0 +1,8 @@
+package ui;
+
+/**
+ *
+ */
+public class MidiPlayer {
+
+}
