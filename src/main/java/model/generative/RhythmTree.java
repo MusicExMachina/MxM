fc08db24cae8251ee8cba727b53d0295cbbc30bc
PatrickCelentano <patrick_celentano@yahoo.com>
2016-09-18 21:38:23 -0400

    Added on to RhythmTree.

--- src/main/java/model/generative/RhythmTree.java
+++ src/main/java/model/generative/RhythmTree.java
@@ -1,9 +1,6 @@
 package model.generative;
 
-import model.structural.Count;
 import model.structural.Frame;
-
-import java.io.Serializable;
 import java.util.*;
 
 /**
@@ -13,23 +10,16 @@ import java.util.*;
  * save that their only "real" storage is in their
  * leaf nodes. This
  */
+public class RhythmTree /* implements Serializable, Cloneable, Iterable<Frame>, Collection<Frame>, NavigableSet<Frame>, Set<Frame>, SortedSet<Frame> */ {
 
-public class RhythmTree implements Serializable, Cloneable, Iterable<Frame>, Collection<Frame>, NavigableSet<Frame>, Set<Frame>, SortedSet<Frame> {
-
-    private class Node {
-        Count timing;
-        Count length;
-        Node parent;
-        List<Node> children;
-    }
-
-    private Node root = null;
+    private RhythmNode root;
+    List<List<RhythmNode>> layers;
 
     /**
      * The RhythmTree default constructor
      */
     public RhythmTree() {
-
+        root = new RhythmNode();
     }
 
     /**
@@ -39,57 +29,21 @@ public class RhythmTree implements Serializable, Cloneable, Iterable<Frame>, Col
     public RhythmTree(RhythmTree other) {
 
     }
+}
 
-    /*   HERE BE OVERRIDE METHODS    */
-    /* ABANDON ALL HOPE YE WHO ENTER */
 
-    @Override
-    public Frame lower(Frame frame) {
-        return null;
-    }
 
-    @Override
-    public Frame floor(Frame frame) {
-        return null;
-    }
 
-    @Override
-    public Frame ceiling(Frame frame) {
-        return null;
-    }
 
-    @Override
-    public Frame higher(Frame frame) {
-        return null;
-    }
 
-    @Override
-    public Frame pollFirst() {
-        return null;
-    }
 
-    @Override
-    public Frame pollLast() {
-        return null;
-    }
 
-    @Override
-    public int size() {
-        return 0;
-    }
 
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
 
-    @Override
-    public boolean contains(Object o) {
-        return false;
-    }
 
-    @Override
-    public Iterator<Frame> iterator() {
+
+//@Override
+//public Iterator<Frame> iterator() {
             /*
         Iterator<Frame> it = new Iterator<Frame>() {
             private Node node = root;
@@ -122,106 +76,5 @@ public class RhythmTree implements Serializable, Cloneable, Iterable<Frame>, Col
         };
         return it;
         */
-        return null;
-    }
-
-    @Override
-    public Object[] toArray() {
-        return new Object[0];
-    }
-
-    @Override
-    public <T> T[] toArray(T[] a) {
-        return null;
-    }
-
-    @Override
-    public boolean add(Frame frame) {
-        return false;
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        return false;
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends Frame> c) {
-        return false;
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public void clear() {
-
-    }
-
-    @Override
-    public NavigableSet<Frame> descendingSet() {
-        return null;
-    }
-
-    @Override
-    public Iterator<Frame> descendingIterator() {
-        return null;
-    }
-
-    @Override
-    public NavigableSet<Frame> subSet(Frame fromElement, boolean fromInclusive, Frame toElement, boolean toInclusive) {
-        return null;
-    }
-
-    @Override
-    public NavigableSet<Frame> headSet(Frame toElement, boolean inclusive) {
-        return null;
-    }
-
-    @Override
-    public NavigableSet<Frame> tailSet(Frame fromElement, boolean inclusive) {
-        return null;
-    }
-
-    @Override
-    public Comparator<? super Frame> comparator() {
-        return null;
-    }
-
-    @Override
-    public SortedSet<Frame> subSet(Frame fromElement, Frame toElement) {
-        return null;
-    }
-
-    @Override
-    public SortedSet<Frame> headSet(Frame toElement) {
-        return null;
-    }
-
-    @Override
-    public SortedSet<Frame> tailSet(Frame fromElement) {
-        return null;
-    }
-
-    @Override
-    public Frame first() {
-        return null;
-    }
-
-    @Override
-    public Frame last() {
-        return null;
-    }
-}
+//   return null;
+//}
