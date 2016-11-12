3124dfabadbb0fea11caebb1707979eb987f2000
Jon Patsenker <jonathan@patsenker.com>
2016-11-12 15:53:27 -0500

    rdy for mrg

--- mxm-main/src/main/java/learning/ninputs/RhythmInput.java
+++ mxm-main/src/main/java/learning/ninputs/RhythmInput.java
@@ -11,7 +11,9 @@ import java.util.List;
 
 public class RhythmInput {
 
-
+    /**
+     * A list of all rhythm trees in training set (IID for NN processing)
+     */
     private List<RhythmTree> rhythms;
 
     /**
@@ -25,6 +27,10 @@ public class RhythmInput {
         rhythms.add(tree);
     }
 
+    /**
+     * Build dl4j DataSet object from this
+     * @return DataSet object with all RhythmInput for training
+     */
     public DataSet buildDataSet(){
 
         ArrayList<ArrayList<Integer>> rhythmLists = new ArrayList<>();
@@ -42,6 +48,8 @@ public class RhythmInput {
         INDArray input = Nd4j.zeros(maxTreeLength, numPossibleStates);
         INDArray output = Nd4j.zeros(1, numPossibleStates);
 
+
+        //Create DS from input and output, put into Matrix
         for (int i = 0; i < rhythmLists.size(); i++) {
             ArrayList<Integer> rhythm = rhythmLists.get(i);
             //Pad the lists with 0s in front (will be removed later)
@@ -49,9 +57,10 @@ public class RhythmInput {
                 rhythm.add(0, 0);
             }
             for (int j = 0; j < rhythm.size(); j++) {
-                input.putScalar(i, jkasdjfl;s);
+                input.putScalar(i, rhythm.get(j));
             }
         }
+        //TODO::List Length Calculation!!!
         return new DataSet(input, output);
     }
 
