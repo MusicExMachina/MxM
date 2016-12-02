4a2aa1675a61b1e1c60f977756e04880a12a7c6c
Jon Patsenker <jonathan@patsenker.com>
2016-12-01 22:19:21 -0500

    Adding SimpleRhythmGenerator

--- mxm-main/src/main/java/learning/experiments/SimpleRhythmGeneration.java
+++ mxm-main/src/main/java/learning/experiments/SimpleRhythmGeneration.java
@@ -1,11 +1,21 @@
 package learning.experiments;
 
+import io.MidiTools;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Sequence;
+import java.io.IOException;
+
 /**
  * Created by jpatsenker on 12/1/16.
  */
 public class SimpleRhythmGeneration {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException, InvalidMidiDataException, MidiUnavailableException {
+        Sequence sequence = MidiTools.download("schubertImpromptu.mid");
+        MidiTools.parse(sequence);
+        MidiTools.play(sequence);
+        System.out.println(sequence.toString());
     }
 }
