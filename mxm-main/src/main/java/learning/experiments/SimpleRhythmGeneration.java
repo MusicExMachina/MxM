0aa3ff2297ad1746835d453b652f972de6958542
Jon Patsenker <jonathan@patsenker.com>
2016-12-01 22:56:22 -0500

    Attaching LSTM to main

--- mxm-main/src/main/java/learning/experiments/SimpleRhythmGeneration.java
+++ mxm-main/src/main/java/learning/experiments/SimpleRhythmGeneration.java
@@ -1,11 +1,19 @@
 package learning.experiments;
 
 import io.MidiTools;
+import learning.ModifiedTextLSTM;
+import model.form.Line;
+import model.form.Passage;
+import model.form.Rhythm;
+import model.rhythmTree.RhythmTree;
 
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Sequence;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by jpatsenker on 12/1/16.
@@ -14,8 +22,27 @@ public class SimpleRhythmGeneration {
 
     public static void main(String[] args) throws IOException, InvalidMidiDataException, MidiUnavailableException {
         Sequence sequence = MidiTools.download("http://www.classicalmidi.co.uk/music2/Pergynt4.mid");
-        MidiTools.parse(sequence);
-        MidiTools.play(sequence);
-        System.out.println(sequence.toString());
+        Passage pass = MidiTools.parse(sequence);
+
+        //MidiTools.play(sequence);
+        //System.out.println(sequence.toString());
+
+        ArrayList<List<Integer>> listOfTrees = new ArrayList<>();
+        for (Line line : pass) {
+            for (RhythmTree rt : line.getRhythm()){
+                listOfTrees.add(rt.toSubdivisionList());
+            }
+        }
+
+        int[][] treeArr = new int[listOfTrees.size()][];
+        for(int i = 0; i<treeArr.length; i++){
+            treeArr[i] = new int[listOfTrees.get(i).size()];
+            for(int j = 0; j<treeArr[i].length; i++){
+                treeArr[i][j] = listOfTrees.get(i).get(j);
+            }
+        }
+
+        ModifiedTextLSTM network = new ModifiedTextLSTM(treeArr,50,2);
+        network.run();
     }
 }
