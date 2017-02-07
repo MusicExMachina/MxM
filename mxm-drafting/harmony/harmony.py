ba10a9119f28bfc04a747f9bbb4ed85b8d9eda5f
Jon Patsenker <jonathan@patsenker.com>
2017-02-07 16:16:49 -0500

    adding some harmony class core structures

---
+++ mxm-drafting/harmony/harmony.py
@@ -0,0 +1,9 @@
+class Harmony:
+"""
+Class for Harmony storage in context of harmony detection
+"""	
+	def __init__(self):
+		self.note_importance = {} #dict for note importance in harmony
+	
+	def addNoteWithImportance(note, importane):
+		self.note_importance[note] = importance
---
+++ mxm-drafting/harmony/note.py
@@ -0,0 +1,5 @@
+class Note:
+
+	def __init__(self, pitch, length):
+		self.pitch = pitch #as pitch-class
+		self.length = length #as float
