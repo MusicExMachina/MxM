f2d26cb888c67c43a074e4e552a120be2f8ee8ec
Jon Patsenker <jonathan@patsenker.com>
2017-02-14 13:32:21 -0500

    set up necessary for learning, started feature extraction

--- mxm-drafting/harmony/main.py
+++ mxm-drafting/harmony/main.py
@@ -5,5 +5,32 @@ from window import Window
 
 
 
+p = Passage()
+
+###PARSE PASSAGE###
+
+
+#create windows of length one beat
+
+
+#create windows of length half measure
+
+
+#create winodws of length full measure
+
+
+#calculate note importance
+
+
+#get coherent windows
+
+
+#create matrix of windows
+
+
+#run k-means
+
+
+#show clusters (PCA or something)
 
 
