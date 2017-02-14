0ef8a1db37844d9f50ece6d96df2843da9382d49
Jon Patsenker <jonathan@patsenker.com>
2017-02-14 13:21:13 -0500

    Finished structure, started parsing/kmeans

--- mxm-drafting/harmony/note.py
+++ mxm-drafting/harmony/note.py
@@ -1,5 +1,14 @@
+def importanceFuncSimple(l, s):
+	return l*s
+
 class Note:
 
-	def __init__(self, pitch, length):
+	def __init__(self, pitch, length, notestart, importanceFunc = importanceFuncSimple):
 		self.pitch = pitch #as pitch-class
 		self.length = length #as float
+		self.notestart = notestart #note start as float
+		self.importanceFunc = importanceFunc
+
+	def get_importance(self):
+		return self.importanceFunc(self.length, self.notestart)
+
