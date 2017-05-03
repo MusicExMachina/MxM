3e446ee50f6bbce05bb7ff3839376f8acbb44a2e
PatrickCelentano <patrick_celentano@yahoo.com>
2017-05-03 13:53:00 -0400

    Basic LilyPond calls implemented.

--- mxm-lilypond/src/main/java/LilyPondTools.java
+++ mxm-lilypond/src/main/java/LilyPondTools.java
@@ -1,5 +1,10 @@
+import javax.sound.midi.InvalidMidiDataException;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Created by celenp on 5/3/2017.
@@ -13,21 +18,46 @@ public class LilyPondTools {
     public static void engrave() {
         try {
             // Run LilyPond
-            String target   = "C:\\Program Files (x86)\\LilyPond\\usr\\bin\\lilypond.exe";
-            Runtime rt      = Runtime.getRuntime();
-            Process proc    = rt.exec(target);
+            String lilyPondPath     = "C:\\Program Files (x86)\\LilyPond\\usr\\bin\\lilypond.exe";
+            String resourcesPath    = "mxm-lilypond/src/tests/resources/";
+            Path curPath            = Paths.get("").toAbsolutePath();
+            Path path               = Paths.get(curPath.toString(), resourcesPath);
+            Runtime runtime         = Runtime.getRuntime();
 
-            // Wait for LilyPond to do its thing
-            proc.waitFor();
+            System.out.println(path.toString());
+            // Get the resources folder
+            File folder = new File(path.toString());
+            File[] allFiles = folder.listFiles();
 
-            // Get the LilyPond's command line output
-            StringBuilder output    = new StringBuilder();
-            BufferedReader reader   = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-            String line;
-            while ((line = reader.readLine())!= null) {
-                output.append(line).append("\n");
+            for (int i = 0; i < allFiles.length; i++) {
+                if (allFiles[i].isFile()) {
+                    String fileName = allFiles[i].getName();
+                    if(fileName.contains(".ly")) {
+                        try {
+                            // Wait for LilyPond to do its thing
+                            String command = lilyPondPath+ " --pdf " + path + "\\" + fileName;
+                            System.out.println(command);
+                            Process process         = runtime.exec(command);
+                            process.waitFor();
+
+                            // Get the LilyPond's command line output
+                            StringBuilder output    = new StringBuilder();
+                            BufferedReader reader   = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+                            String line;
+                            while ((line = reader.readLine())!= null) {
+                                output.append(line).append("\n");
+                            }
+                            System.out.println("LilyPond: " + output);
+
+                        } catch (IOException e) {
+                            System.err.println("Could not open "+fileName+"!");
+                            System.err.println(e);
+                        }
+                    }
+                }
             }
-            System.out.println("### " + output);
+
+
 
 
             System.out.println("Completed!");
