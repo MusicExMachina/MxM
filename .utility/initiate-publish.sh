a88a6f773cf97e220f7bfa35a38d74cba792be0f
PatrickCelentano <patrick_celentano@yahoo.com>
2017-08-13 17:04:44 -0400

    -

--- .utility/initiate-publish.sh
+++ .utility/initiate-publish.sh
@@ -3,7 +3,9 @@
 # NOTE: Travis-CI can only publish SNAPSHOT versions. To release a version, you need
 #       to use the internal ReadyTalk Jenkins job.
 
-if [ "$TRAVIS_REPO_SLUG" == "ReadyTalk/swt-bling" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+echo -e "ABCDEFG\n"
+
+if [ "$TRAVIS_REPO_SLUG" == "MusicExMachina/MxM" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
   if [[ $(./gradlew -q getVersion) != *SNAPSHOT* ]]; then
       echo 'Travis can only publish snapshots. To publish a release, use the ReadyTalk Jenkins instance.'
       return 0
