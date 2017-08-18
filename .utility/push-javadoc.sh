0385d7b7d10bd52ee4c70dad5b0c59fc07aa06d0
PatrickCelentano <patrick_celentano@yahoo.com>
2017-08-17 22:14:06 -0400

    -

--- .utility/push-javadoc.sh
+++ .utility/push-javadoc.sh
@@ -15,7 +15,7 @@ if [ "$TRAVIS_REPO_SLUG" == "MusicExMachina/MxM" ] &&
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}:x-oauth-basic@github.com/MusicExMachina/MxM gh-pages > /dev/null
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/MusicExMachina/MxM.git gh-pages > /dev/null
 
   # Commit and Push the Changes
   cd gh-pages
