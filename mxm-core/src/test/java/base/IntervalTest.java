2eaceddbbeb97ac67c715c9678333edc8d11b2f0
PatrickCelentano <patrick_celentano@yahoo.com>
2017-08-28 22:31:49 -0400

    More documentation!

---
+++ mxm-core/src/test/java/base/IntervalTest.java
@@ -0,0 +1,47 @@
+package base;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IntervalTest {
+    @Test
+    void iterator() {
+    }
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void getIntervalClass() {
+    }
+
+    @Test
+    void getOctaves() {
+    }
+
+    @Test
+    void getSize() {
+    }
+
+    @Test
+    void plus() {
+    }
+
+    @Test
+    void minus() {
+    }
+
+    @Test
+    void compareTo() {
+    }
+
+    @Test
+    void compare() {
+    }
+
+    @Test
+    void equals() {
+    }
+}
